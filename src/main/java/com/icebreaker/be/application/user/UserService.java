5f369a8853549f4432a5881e17bc2780993dff7b
KNU-K <knukang334@gmail.com>
2025-08-28 05:02:34 +0900

    feat(user): User 비즈니스 로직 추가
    - 사용자 생성 ( 존재하지않으면 생성)
    - 사용자 단건 조회
    - 사용자 전체 조회
    - 사용자 삭제

---
+++ src/main/java/com/icebreaker/be/application/user/UserService.java
@@ -0,0 +1,58 @@
+package com.icebreaker.be.application.user;
+
+import com.icebreaker.be.application.user.dto.CreateUserCommand;
+import com.icebreaker.be.application.user.dto.UserResponse;
+import com.icebreaker.be.application.user.mapper.UserMapper;
+import com.icebreaker.be.domain.user.User;
+import com.icebreaker.be.domain.user.UserRepository;
+import com.icebreaker.be.global.exception.BusinessException;
+import com.icebreaker.be.global.exception.ErrorCode;
+import java.util.List;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
+    public UserResponse getUserById(Long id) {
+        return userRepository.findById(id)
+                .map(UserMapper::toResponse)
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+    }
+
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
+    public List<UserResponse> getAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .filter(Objects::nonNull)
+                .map(UserMapper::toResponse)
+                .toList();
+    }
+
+    @Transactional
+    public Long createUserIfNotExists(CreateUserCommand cmd) {
+        return userRepository.findByPhone(cmd.phone())
+                .map(User::getId)
+                .orElseGet(() -> createUser(cmd.toEntity()));
+    }
+
+    @Transactional
+    public void deleteUser(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+        userRepository.delete(user);
+    }
+
+    private Long createUser(User user) {
+        return userRepository.save(user).getId();
+    }
+}
