809bbcff750e28f936a23b458b24bf0a178b1ef8
KNU-K <knukang334@gmail.com>
2025-08-28 05:13:37 +0900

    feat(user): 추가적인 에러 코드 정의

--- src/main/java/com/icebreaker/be/global/exception/ErrorCode.java
+++ src/main/java/com/icebreaker/be/global/exception/ErrorCode.java
@@ -11,7 +11,10 @@ public enum ErrorCode {
 
     // USER ERROR
     USER_NOT_FOUND("사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    INVALID_MBTI_TYPE("유효하지 않은 MBTI 타입입니다.", HttpStatus.BAD_REQUEST);
+    INVALID_MBTI_TYPE("유효하지 않은 MBTI 타입입니다.", HttpStatus.BAD_REQUEST),
+
+    INVALID_REQUEST("유효하지 않은 요청입니다.", HttpStatus.BAD_REQUEST),
+    ;
 
     private final String message;
     private final HttpStatus status;
