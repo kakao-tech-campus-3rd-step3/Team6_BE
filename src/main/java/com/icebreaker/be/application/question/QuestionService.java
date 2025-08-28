35fa767607a39d04ab3bcbd9869b33b429949fec
eastgyu11 <eastgyu11@naver.com>
2025-08-28 23:52:34 +0900

    feat(question): Question 서비스 추가 - 질문 단건 조회 - 질문 전체 조회 - 질문 생성 - 질문 삭제

---
+++ src/main/java/com/icebreaker/be/application/question/QuestionService.java
@@ -0,0 +1,48 @@
+package com.icebreaker.be.application.question;
+
+import com.icebreaker.be.application.question.dto.CreateQuestionDto;
+import com.icebreaker.be.application.question.dto.QuestionResponseDto;
+import com.icebreaker.be.application.question.mapper.QuestionMapper;
+import com.icebreaker.be.domain.question.QuestionRepository;
+import com.icebreaker.be.global.exception.BusinessException;
+import com.icebreaker.be.global.exception.ErrorCode;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class QuestionService {
+
+  private final QuestionRepository questionRepository;
+
+  @Transactional(readOnly = true)
+  public QuestionResponseDto getQuestionById(Long id) {
+    return questionRepository
+        .findById(id)
+        .map(QuestionMapper::toResponse)
+        .orElseThrow(() -> new BusinessException(ErrorCode.QUESTION_NOT_FOUND));
+  }
+
+  @Transactional(readOnly = true)
+  public List<QuestionResponseDto> getAllQuestions() {
+    return questionRepository
+        .findAll()
+        .stream()
+        .map(QuestionMapper::toResponse)
+        .toList();
+  }
+
+  @Transactional
+  public Long createQuestion(CreateQuestionDto createQuestionDto) {
+    return questionRepository.save(createQuestionDto.toEntity()).getId();
+  }
+
+  @Transactional
+  public void deleteQuestion(Long id) {
+    questionRepository.deleteById(id);
+  }
+}
