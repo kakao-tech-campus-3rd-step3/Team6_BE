b02779434e36f254e2f57a9d44554b0749513c20
eastgyu11 <eastgyu11@naver.com>
2025-08-28 23:48:10 +0900

    feat(question): Question 생성, 응답 DTO 추가

---
+++ src/main/java/com/icebreaker/be/application/question/dto/CreateQuestionDto.java
@@ -0,0 +1,26 @@
+package com.icebreaker.be.application.question.dto;
+
+import com.icebreaker.be.domain.question.Question;
+import com.icebreaker.be.domain.question.QuestionType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateQuestionDto(
+    @NotBlank
+    @Size(max = 255)
+    @Schema(description = "질문 내용", example = "어떤 음식을 좋아하시나요?", maxLength = 255)
+    String content,
+
+    @NotBlank
+    @Schema(description = "질문 유형", example = "공통")
+    QuestionType type
+) {
+
+  public Question toEntity() {
+    return Question.builder()
+        .content(content)
+        .type(type)
+        .build();
+  }
+}
