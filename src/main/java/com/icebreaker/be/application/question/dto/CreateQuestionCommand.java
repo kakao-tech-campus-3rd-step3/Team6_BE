425445bd50a3d2d490b18a693ecf6236ad5d4d38
KNU-K <knukang334@gmail.com>
2025-08-30 04:58:41 +0900

    refactor: change validation from @NotBlank to @NotNull for content and questionType fields

--- src/main/java/com/icebreaker/be/application/question/dto/CreateQuestionCommand.java
+++ src/main/java/com/icebreaker/be/application/question/dto/CreateQuestionCommand.java
@@ -3,17 +3,17 @@ package com.icebreaker.be.application.question.dto;
 import com.icebreaker.be.domain.question.Question;
 import com.icebreaker.be.domain.question.QuestionType;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record CreateQuestionCommand(
-        @NotBlank
+        @NotNull
         @Size(max = 255)
         @Schema(description = "질문 내용", example = "어떤 음식을 좋아하시나요?", maxLength = 255)
         String content,
 
-        @NotBlank
+        @NotNull
         @Pattern(regexp = "^[가-힣]*$", message = "질문 유형은 한글만 입력 가능합니다.")
         @Schema(description = "질문 유형", example = "공통")
         String questionType
