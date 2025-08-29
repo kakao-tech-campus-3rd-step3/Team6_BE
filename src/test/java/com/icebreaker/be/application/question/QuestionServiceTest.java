9a7a59e79bdbc1dc903581ddb2b45ec6087bf60c
eastgyu11 <eastgyu11@naver.com>
2025-08-29 22:14:08 +0900

    test: questionType필드 유효성 검사에 따른 인자 값 수정

--- src/test/java/com/icebreaker/be/application/question/QuestionServiceTest.java
+++ src/test/java/com/icebreaker/be/application/question/QuestionServiceTest.java
@@ -85,7 +85,7 @@ class QuestionServiceTest {
         Long id = 3L;
         Question saved = new Question("content", QuestionType.COMMON);
         ReflectionTestUtils.setField(saved, "id", id);
-        CreateQuestionCommand command = new CreateQuestionCommand("content", "common");
+        CreateQuestionCommand command = new CreateQuestionCommand("content", "공통");
 
         when(questionRepository.save(any(Question.class))).thenReturn(saved);
 
