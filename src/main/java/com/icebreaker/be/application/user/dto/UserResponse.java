ce42bc2e8f55fcaca4651150e06280f6d7ed4428
KNU-K <knukang334@gmail.com>
2025-08-30 04:16:20 +0900

    feat: update UserResponse to use Set<Interest> for user interests

--- src/main/java/com/icebreaker/be/application/user/dto/UserResponse.java
+++ src/main/java/com/icebreaker/be/application/user/dto/UserResponse.java
@@ -1,16 +1,17 @@
 package com.icebreaker.be.application.user.dto;
 
-import com.icebreaker.be.domain.interest.InterestType;
+import com.icebreaker.be.domain.user.Interest;
 import com.icebreaker.be.domain.user.MbtiType;
+import java.util.Set;
 
 public record UserResponse(
         Long id,
         String name,
         String phone,
         Integer age,
-        String[] interests,
         MbtiType mbti,
-        String introduction
+        String introduction,
+        Set<Interest> interests
 ) {
 
 }
