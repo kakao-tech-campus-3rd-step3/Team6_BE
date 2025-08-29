cd143d49706f27945cfc11a4c1f386d6ede19a9c
IT-Gentleman <gentleman@kakao.com>
2025-08-29 21:31:14 +0900

    feat(error): Change INVALID_INTEREST_TYPE error code to BAD_REQUEST

--- src/main/java/com/icebreaker/be/global/exception/ErrorCode.java
+++ src/main/java/com/icebreaker/be/global/exception/ErrorCode.java
@@ -14,7 +14,7 @@ public enum ErrorCode {
     INVALID_MBTI_TYPE("유효하지 않은 MBTI 타입입니다.", HttpStatus.BAD_REQUEST),
 
     // INTEREST ERROR
-    INVALID_INTEREST_TYPE("유효하지 않은 관심사 타입입니다.", HttpStatus.NOT_FOUND),
+    INVALID_INTEREST_TYPE("유효하지 않은 관심사 타입입니다.", HttpStatus.BAD_REQUEST),
 
     INVALID_REQUEST("유효하지 않은 요청입니다.", HttpStatus.BAD_REQUEST),
     ;
