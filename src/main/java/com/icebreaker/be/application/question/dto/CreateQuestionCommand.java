b29a8777878f9be5e73f75f97aa66339dddb8843
eastgyu11 <eastgyu11@naver.com>
2025-08-29 21:54:36 +0900

    refactor: questionType필드 한글만 입력받도록 유효성 검사

--- src/main/java/com/icebreaker/be/application/question/dto/CreateQuestionCommand.java
+++ src/main/java/com/icebreaker/be/application/question/dto/CreateQuestionCommand.java
@@ -4,6 +4,7 @@ import com.icebreaker.be.domain.question.Question;
 import com.icebreaker.be.domain.question.QuestionType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record CreateQuestionCommand(
@@ -11,8 +12,9 @@ public record CreateQuestionCommand(
         @Size(max = 255)
         @Schema(description = "질문 내용", example = "어떤 음식을 좋아하시나요?", maxLength = 255)
         String content,
-        
+
         @NotBlank
+        @Pattern(regexp = "^[가-힣]*$", message = "질문 유형은 한글만 입력 가능합니다.")
         @Schema(description = "질문 유형", example = "공통")
         String questionType
 ) {
