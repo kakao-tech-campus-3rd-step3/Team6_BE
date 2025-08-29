b0531056da9d2fd68bdffa0a317270327981c463
eastgyu11 <eastgyu11@naver.com>
2025-08-29 21:58:25 +0900

    refactor: enum변환 로직을 한글입력에 맞게 수정

--- src/main/java/com/icebreaker/be/domain/question/QuestionType.java
+++ src/main/java/com/icebreaker/be/domain/question/QuestionType.java
@@ -6,10 +6,16 @@ import com.icebreaker.be.global.exception.ErrorCode;
 public enum QuestionType {
     COMMON, PERSONAL;
 
-    public static QuestionType fromString(String questionTypeString) {
-        try {
-            return QuestionType.valueOf(questionTypeString.toUpperCase());
-        } catch (IllegalArgumentException | NullPointerException e) {
+    public static QuestionType fromString(String questionType) {
+        if (questionType == null) {
+            throw new BusinessException(ErrorCode.INVALID_QUESTION_TYPE);
+        }
+
+        if (questionType.equals("공통")) {
+            return COMMON;
+        } else if (questionType.equals("개인")) {
+            return PERSONAL;
+        } else {
             throw new BusinessException(ErrorCode.INVALID_QUESTION_TYPE);
         }
     }
