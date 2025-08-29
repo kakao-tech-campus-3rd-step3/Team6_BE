9d2e691b532eb249144b18e19cdf951f019e9921
KNU-K <knukang334@gmail.com>
2025-08-29 19:22:26 +0900

    fix: correct grammar in Questions class description

--- src/main/java/com/icebreaker/be/infra/llm/assisstant/Questions.java
+++ src/main/java/com/icebreaker/be/infra/llm/assisstant/Questions.java
@@ -4,7 +4,7 @@ import dev.langchain4j.model.output.structured.Description;
 import java.util.List;
 
 public record Questions(
-        @Description("generate a list of questions consist of 5 that is related to the given user text")
+        @Description("generate a list of questions consisting of 5 that are related to the given user text")
         List<String> values
 ) {
 
