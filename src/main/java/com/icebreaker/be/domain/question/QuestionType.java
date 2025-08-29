e15a02505028d3fb565783a348152b2129ad0185
KNU-K <knukang334@gmail.com>
2025-08-30 04:53:34 +0900

    refactor: QuestionType을 Stream형태로 변환

--- src/main/java/com/icebreaker/be/domain/question/QuestionType.java
+++ src/main/java/com/icebreaker/be/domain/question/QuestionType.java
@@ -2,33 +2,22 @@ package com.icebreaker.be.domain.question;
 
 import com.icebreaker.be.global.exception.BusinessException;
 import com.icebreaker.be.global.exception.ErrorCode;
+import java.util.Arrays;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@Getter
+@RequiredArgsConstructor
 public enum QuestionType {
     COMMON("공통"),
     PERSONAL("개인");
 
     private final String value;
 
-    QuestionType(String value) {
-        this.value = value;
-    }
-
     public static QuestionType fromString(String questionType) {
-        if (questionType == null) {
-            throw new BusinessException(ErrorCode.INVALID_QUESTION_TYPE);
-        }
-
-        // value(): enum에 정의된 모든 상수를 배열형태로 반환
-        for (QuestionType type : values()) {
-            if (type.value.equals(questionType)) {
-                return type;
-            }
-        }
-
-        throw new BusinessException(ErrorCode.INVALID_QUESTION_TYPE);
-    }
-
-    public String getValue() {
-        return value;
+        return Arrays.stream(values())
+                .filter(t -> t.value != null && t.value.equals(questionType))
+                .findFirst()
+                .orElseThrow(() -> new BusinessException(ErrorCode.INVALID_QUESTION_TYPE));
     }
 }
