10b825de7da591fedab948a206d188281b285a94
KNU-K <knukang334@gmail.com>
2025-09-01 20:29:50 +0900

    chore: refactor InMemoryWaitingRoomIdGenerator and InMemoryWaitingRoomRepository packages for better organization

--- src/main/java/com/icebreaker/be/infra/generator/InMemoryWaitingRoomIdGenerator.java
+++ src/main/java/com/icebreaker/be/infra/generator/InMemoryWaitingRoomIdGenerator.java
@@ -1,8 +1,9 @@
-package com.icebreaker.be.infra;
+package com.icebreaker.be.infra.generator;
 
 import com.icebreaker.be.domain.room.WaitingRoomIdGenerator;
-import java.util.UUID;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 public class InMemoryWaitingRoomIdGenerator implements WaitingRoomIdGenerator {
--- src/main/java/com/icebreaker/be/infra/persistence/InMemoryWaitingRoomRepository.java
+++ src/main/java/com/icebreaker/be/infra/persistence/InMemoryWaitingRoomRepository.java
@@ -1,11 +1,12 @@
-package com.icebreaker.be.infra;
+package com.icebreaker.be.infra.persistence;
 
 import com.icebreaker.be.domain.room.WaitingRoom;
 import com.icebreaker.be.domain.room.WaitingRoomRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
-import org.springframework.stereotype.Repository;
 
 @Repository
 public class InMemoryWaitingRoomRepository implements WaitingRoomRepository {
