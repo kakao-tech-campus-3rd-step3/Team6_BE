92db0e07f7d954b2f629eb68c102d640ebf83d10
KNU-K <knukang334@gmail.com>
2025-09-01 19:56:10 +0900

    feat: add CurrentUser annotation and resolver for handling user identification

---
+++ src/main/java/com/icebreaker/be/global/resolver/CurrentUserArgumentResolver.java
@@ -0,0 +1,38 @@
+package com.icebreaker.be.global.resolver;
+
+import com.icebreaker.be.global.annotation.CurrentUser;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+@Slf4j
+public class CurrentUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(CurrentUser.class)
+                && parameter.getParameterType().equals(Long.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, Message<?> message) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+
+        if (accessor.getUser() != null) {
+            return Long.parseLong(accessor.getUser().getName());
+        }
+
+        Map<String, Object> sessionAttributes = accessor.getSessionAttributes();
+        if (sessionAttributes != null && sessionAttributes.get("userId") != null) {
+            return Long.parseLong(sessionAttributes.get("userId").toString());
+        }
+
+        throw new RuntimeException("JWT가 유효하지 않거나 userId가 없음");
+    }
+}
