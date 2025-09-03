2b3daadc5b8202ae8f86e1d494e40de6dcb0c38c
KNU-K <knukang334@gmail.com>
2025-09-03 20:59:47 +0900

    refactor: enhance Redis serializer configuration with ObjectMapper integration

--- src/main/java/com/icebreaker/be/infra/config/RedisConfig.java
+++ src/main/java/com/icebreaker/be/infra/config/RedisConfig.java
@@ -1,6 +1,8 @@
 package com.icebreaker.be.infra.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.icebreaker.be.domain.room.WaitingRoom;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -9,7 +11,9 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@RequiredArgsConstructor
 public class RedisConfig {
+    private final ObjectMapper objectMapper;
 
     @Bean
     public RedisTemplate<String, WaitingRoom> redisTemplate(LettuceConnectionFactory connectionFactory) {
@@ -18,7 +22,7 @@ public class RedisConfig {
 
         template.setKeySerializer(new StringRedisSerializer());
 
-        Jackson2JsonRedisSerializer<WaitingRoom> serializer = new Jackson2JsonRedisSerializer<>(WaitingRoom.class);
+        Jackson2JsonRedisSerializer<WaitingRoom> serializer = new Jackson2JsonRedisSerializer<>(objectMapper, WaitingRoom.class);
         template.setValueSerializer(serializer);
         template.setHashValueSerializer(serializer);
 
