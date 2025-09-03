eacc3f676129c43d1b528753b84d870ba738e6e7
KNU-K <knukang334@gmail.com>
2025-09-03 20:49:10 +0900

    feat: refactor WaitingRoom class for Redis compatibility and improve participant management

--- src/main/java/com/icebreaker/be/domain/room/WaitingRoom.java
+++ src/main/java/com/icebreaker/be/domain/room/WaitingRoom.java
@@ -1,32 +1,39 @@
 package com.icebreaker.be.domain.room;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.io.Serializable;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
-import lombok.Getter;
 
 @Getter
-public class WaitingRoom {
+@NoArgsConstructor
+@RedisHash(value = "waitingRoom", timeToLive = 3600)
+public class WaitingRoom implements Serializable {
 
-    private final String roomId;
-    private final int capacity;
-    private final Set<Long> participants = new HashSet<>();
+    @Id
+    private String roomId;
 
-    private final String name;
+    private String name;
+
+    private int capacity;
+
+    private Set<Long> participants = new HashSet<>();
 
     public WaitingRoom(String roomId, String name, int capacity) {
         this.roomId = roomId;
         this.name = name;
         this.capacity = capacity;
+        this.participants = new HashSet<>();
     }
 
     public synchronized WaitingRoomStatus join(Long userId) {
         participants.add(userId);
-        if (!isFull()) {
-            return WaitingRoomStatus.WAITING;
-        } else {
-            return WaitingRoomStatus.FULL;
-        }
+        return isFull() ? WaitingRoomStatus.FULL : WaitingRoomStatus.WAITING;
     }
 
     public boolean isFull() {
