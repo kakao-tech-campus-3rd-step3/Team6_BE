f6d8fb532b860e2f0b76781ee298d684b35dc66d
KNU-K <knukang334@gmail.com>
2025-09-03 21:04:54 +0900

    refactor: transition RedisWaitingRoomRepository to implement WaitingRoomRepositoryCustom

--- src/main/java/com/icebreaker/be/infra/persistence/RedisWaitingRoomRepository.java
+++ src/main/java/com/icebreaker/be/infra/persistence/RedisWaitingRoomRepository.java
@@ -1,33 +1,9 @@
 package com.icebreaker.be.infra.persistence;
 
-import com.icebreaker.be.domain.room.WaitingRoom;
-import com.icebreaker.be.domain.room.WaitingRoomRepository;
-import org.springframework.data.redis.core.RedisTemplate;
+import com.icebreaker.be.domain.room.WaitingRoomRepositoryCustom;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public class RedisWaitingRoomRepository implements WaitingRoomRepository {
-
-    private final RedisTemplate<String, WaitingRoom> redisTemplate;
-
-    public RedisWaitingRoomRepository(RedisTemplate<String, WaitingRoom> redisTemplate) {
-        this.redisTemplate = redisTemplate;
-    }
-
-    @Override
-    public void save(WaitingRoom waitingRoom) {
-        redisTemplate.opsForValue().set(waitingRoom.getRoomId(), waitingRoom);
-    }
-
-    @Override
-    public Optional<WaitingRoom> findById(String roomId) {
-        return Optional.ofNullable(redisTemplate.opsForValue().get(roomId));
-    }
-
-    @Override
-    public void delete(String roomId) {
-        redisTemplate.delete(roomId);
-    }
+public class RedisWaitingRoomRepository implements WaitingRoomRepositoryCustom {
+    //TODO: 추후 LUA 관련 코드 작성 예정
 }
