95fc9ebd68d53f0f035f16db7dc0446f5cc9a576
KNU-K <knukang334@gmail.com>
2025-09-05 08:32:20 +0900

    feat: simplify RedisScriptEnum by removing redundant script initialization

--- src/main/java/com/icebreaker/be/infra/persistence/redis/RedisScriptEnum.java
+++ src/main/java/com/icebreaker/be/infra/persistence/redis/RedisScriptEnum.java
@@ -1,28 +1,14 @@
 package com.icebreaker.be.infra.persistence.redis;
 
 import lombok.Getter;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.redis.core.script.RedisScript;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum RedisScriptEnum {
 
     CREATE_ROOM("lua/create_room.lua"),
     JOIN_ROOM("lua/join_room.lua");
 
     private final String classpath;
-    private final RedisScript<String> redisScript;
-
-    RedisScriptEnum(String classpath) {
-        this.classpath = classpath;
-        this.redisScript = createRedisScript(classpath);
-    }
-
-    private static RedisScript<String> createRedisScript(String path) {
-        try {
-            return RedisScript.of(new ClassPathResource(path), String.class);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to load Redis script: " + path, e);
-        }
-    }
 }
