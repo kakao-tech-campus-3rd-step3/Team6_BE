69f251ef4b9cf8c7173637e6fc0fd15adb934f1e
KNU-K <knukang334@gmail.com>
2025-09-05 07:05:46 +0900

    feat: rename RoomRepository package to com.icebreaker.be.domain.room.repo

--- src/main/java/com/icebreaker/be/domain/room/repo/RoomRepository.java
+++ src/main/java/com/icebreaker/be/domain/room/repo/RoomRepository.java
@@ -1,5 +1,6 @@
-package com.icebreaker.be.domain.room;
+package com.icebreaker.be.domain.room.repo;
 
+import com.icebreaker.be.domain.room.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
