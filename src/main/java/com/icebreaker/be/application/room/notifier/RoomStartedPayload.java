6612be0da1000f871d82a6bc8818170baedcbf82
KNU-K <knukang334@gmail.com>
2025-09-08 07:26:17 +0900

    feat: simplify RoomStartedPayload by implementing WaitingRoomMessage interface

--- src/main/java/com/icebreaker/be/application/room/notifier/RoomStartedPayload.java
+++ src/main/java/com/icebreaker/be/application/room/notifier/RoomStartedPayload.java
@@ -1,10 +1,7 @@
 package com.icebreaker.be.application.room.notifier;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public record RoomStartedPayload(
-        @JsonProperty("type") WaitingRoomMessageType type,
-        @JsonProperty("roomId") String roomId
-) {
+        String roomId
+) implements WaitingRoomMessage {
 
 }
