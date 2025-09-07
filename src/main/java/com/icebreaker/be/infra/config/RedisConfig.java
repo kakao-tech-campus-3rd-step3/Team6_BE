0adb80bc5fad3b804461f23a1bf2195824dd0cc3
KNU-K <knukang334@gmail.com>
2025-09-08 07:37:16 +0900

    refactor: simplify Redis configuration by removing unused ObjectMapper and related serializers

--- src/main/java/com/icebreaker/be/infra/config/RedisConfig.java
+++ src/main/java/com/icebreaker/be/infra/config/RedisConfig.java
@@ -1,37 +1,13 @@
 package com.icebreaker.be.infra.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@RequiredArgsConstructor
 public class RedisConfig {
-
-    private final ObjectMapper objectMapper;
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(
-            LettuceConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-
-        template.setKeySerializer(new StringRedisSerializer());
-
-        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(
-                objectMapper, Object.class);
-        template.setValueSerializer(serializer);
-        template.setHashValueSerializer(serializer);
-
-        template.afterPropertiesSet();
-        return template;
-    }
 
     @Bean
     public RedisTemplate<String, String> customStringRedisTemplate(
