0b4dac1aae0b8a69e5efe6769893300cc8e6ef0f
KNU-K <knukang334@gmail.com>
2025-09-09 07:51:46 +0900

    feat: simplify RoomService by removing RoomStageRepository and updating stage handling

--- src/main/java/com/icebreaker/be/application/room/RoomService.java
+++ src/main/java/com/icebreaker/be/application/room/RoomService.java
@@ -4,10 +4,7 @@ package com.icebreaker.be.application.room;
 import com.icebreaker.be.application.room.dto.ChangeRoomStageCommand;
 import com.icebreaker.be.application.room.event.RoomEventPublisher;
 import com.icebreaker.be.domain.room.entity.Room;
-import com.icebreaker.be.domain.room.entity.RoomStage;
-import com.icebreaker.be.domain.room.entity.Stage;
 import com.icebreaker.be.domain.room.repo.RoomRepository;
-import com.icebreaker.be.domain.room.repo.RoomStageRepository;
 import com.icebreaker.be.domain.user.User;
 import com.icebreaker.be.domain.user.UserRepository;
 import com.icebreaker.be.domain.waitingroom.WaitingRoom;
@@ -22,7 +19,6 @@ public class RoomService {
 
     private final RoomRepository roomRepository;
     private final UserRepository userRepository;
-    private final RoomStageRepository roomStageRepository;
 
     private final RoomEventPublisher publisher;
 
@@ -33,19 +29,12 @@ public class RoomService {
         room.joinUsers(users);
         Room savedRoom = roomRepository.save(room);
 
-        RoomStage roomStage = RoomStage.init(room.getCode());
-        roomStageRepository.save(roomStage);
-
-        publisher.publishStageChanged(room.getCode(), roomStage.currentStage());
+        publisher.publishStageInitialized(room.getCode());
 
         return savedRoom;
     }
 
     public void changeStage(String roomCode, ChangeRoomStageCommand command) {
-        Stage newStage = command.getStageEnum();
-        RoomStage roomStage = RoomStage.of(roomCode, newStage);
-        roomStageRepository.save(roomStage);
-
-        publisher.publishStageChanged(roomCode, newStage);
+        publisher.publishStageChanged(roomCode, command.getStageEnum());
     }
 }
