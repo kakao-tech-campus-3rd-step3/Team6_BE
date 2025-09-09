38fc50dd85d21c7bea85a80a393ff64deeeb78ce
KNU-K <knukang334@gmail.com>
2025-09-09 11:55:48 +0900

    feat: refactor WaitingRoom handling to support multiple participants and add logging

--- src/main/java/com/icebreaker/be/application/room/event/RoomEventPublisher.java
+++ src/main/java/com/icebreaker/be/application/room/event/RoomEventPublisher.java
@@ -2,9 +2,11 @@ package com.icebreaker.be.application.room.event;
 
 import com.icebreaker.be.domain.room.entity.Stage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class RoomEventPublisher {
