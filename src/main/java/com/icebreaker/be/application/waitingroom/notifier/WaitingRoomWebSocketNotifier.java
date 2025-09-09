2d242e0e9dd34f4130f856d050bc8fdd08d91b40
KNU-K <knukang334@gmail.com>
2025-09-09 11:53:37 +0900

    feat: update participant joined payload to support multiple participants and enhance error logging

--- src/main/java/com/icebreaker/be/application/waitingroom/notifier/WaitingRoomWebSocketNotifier.java
+++ src/main/java/com/icebreaker/be/application/waitingroom/notifier/WaitingRoomWebSocketNotifier.java
@@ -1,10 +1,13 @@
 package com.icebreaker.be.application.waitingroom.notifier;
 
-import com.icebreaker.be.domain.waitingroom.WaitingRoomParticipant;
+import com.icebreaker.be.domain.waitingroom.WaitingRoomWithParticipants.Participant;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class WaitingRoomWebSocketNotifier {
@@ -13,8 +16,8 @@ public class WaitingRoomWebSocketNotifier {
 
     private final SimpMessagingTemplate messagingTemplate;
 
-    public void notifyParticipantJoined(String roomId, WaitingRoomParticipant newParticipant) {
-        WaitingRoomMessage message = new ParticipantJoinedPayload(newParticipant);
+    public void notifyParticipantJoined(String roomId, List<Participant> participants) {
+        WaitingRoomMessage message = new ParticipantJoinedPayload(participants);
         sendToWaitingRoom(roomId, message);
     }
 
