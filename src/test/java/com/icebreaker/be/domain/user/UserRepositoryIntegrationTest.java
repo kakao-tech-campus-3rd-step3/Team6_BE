cb335698e512553a21c5673eb2376f28777e5a6c
IT-Gentleman <gentleman@kakao.com>
2025-09-12 18:07:57 +0900

    chore(tests): add TestPropertySource to UserRepositoryIntegrationTest for consistency

--- src/test/java/com/icebreaker/be/domain/user/UserRepositoryIntegrationTest.java
+++ src/test/java/com/icebreaker/be/domain/user/UserRepositoryIntegrationTest.java
@@ -11,9 +11,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
 
 @DataJpaTest
 @Import({TestLlmConfig.class, TestAuditingConfig.class})
+@TestPropertySource(properties = {
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.datasource.url=jdbc:h2:mem:testdb"
+})
 @DisplayName("UserRepository 통합 테스트")
 class UserRepositoryIntegrationTest {
 
