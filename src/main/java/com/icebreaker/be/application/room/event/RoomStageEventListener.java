d4ec7e50ec03c1e156efabe4a1d81a6a2cded69e
eastgyu11 <eastgyu11@naver.com>
2025-09-21 14:28:21 +0900

    feat: EventListener를 Redis Pub/Sub으로 수정

--- src/main/java/com/icebreaker/be/application/room/event/RoomStageEventListener.java
+++ src/main/java/com/icebreaker/be/application/room/event/RoomStageEventListener.java
@@ -3,7 +3,9 @@ package com.icebreaker.be.application.room.event;
 
 import com.icebreaker.be.application.room.RoomStageService;
 import com.icebreaker.be.global.annotation.AsyncTransactionalEventListener;
-import com.icebreaker.be.infra.messaging.room.RoomStageWebSocketNotifier;
+import com.icebreaker.be.infra.persistence.redis.message.RoomStageChangeMessage;
+import com.icebreaker.be.infra.persistence.redis.message.redisMessage;
+import com.icebreaker.be.infra.persistence.redis.publisher.RedisPublisher;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -15,11 +17,17 @@ import org.springframework.transaction.event.TransactionPhase;
 public class RoomStageEventListener {
 
     private final RoomStageService roomStageService;
-    private final RoomStageWebSocketNotifier notifier;
+    private final RedisPublisher redisPublisher;
 
     @AsyncTransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleRoomStageChangeEvent(RoomStageChangeEvent event) {
         roomStageService.changeStage(event.roomCode(), event.stage());
-        notifier.notifyRoomStageChanged(event.roomCode(), event.stage());
+        
+        RoomStageChangeMessage payload = new RoomStageChangeMessage(event.roomCode(),
+                event.stage());
+        redisMessage<RoomStageChangeMessage> message = new redisMessage<>(
+                "ROOM_STAGE_CHANGE", payload);
+        redisPublisher.publish(message);
+        log.info("ROOM_STAGE_CHANGE message publish to redis");
     }
 }
--- src/main/java/com/icebreaker/be/application/waitingroom/event/WaitingRoomEventListener.java
+++ src/main/java/com/icebreaker/be/application/waitingroom/event/WaitingRoomEventListener.java
@@ -4,6 +4,10 @@ import com.icebreaker.be.application.room.RoomService;
 import com.icebreaker.be.domain.waitingroom.WaitingRoom;
 import com.icebreaker.be.global.annotation.AsyncTransactionalEventListener;
 import com.icebreaker.be.infra.messaging.waitingroom.WaitingRoomWebSocketNotifier;
+import com.icebreaker.be.infra.persistence.redis.message.ParticipantJoinedMessage;
+import com.icebreaker.be.infra.persistence.redis.message.RoomStartedMessage;
+import com.icebreaker.be.infra.persistence.redis.message.redisMessage;
+import com.icebreaker.be.infra.persistence.redis.publisher.RedisPublisher;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,12 +20,18 @@ import org.springframework.transaction.event.TransactionPhase;
 public class WaitingRoomEventListener {
 
     private final WaitingRoomWebSocketNotifier notifier;
-
     private final RoomService roomService;
+    private final RedisPublisher redisPublisher;
 
     @AsyncTransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleWaitingRoomParticipantJoinedEvent(WaitingRoomParticipantJoinedEvent event) {
-        notifier.notifyParticipantJoined(event.roomId(), event.waitingRoomWithParticipants());
+
+        ParticipantJoinedMessage payload = new ParticipantJoinedMessage(event.roomId(),
+                event.waitingRoomWithParticipants());
+        redisMessage<ParticipantJoinedMessage> message = new redisMessage<>(
+                "PARTICIPANT_JOINED", payload);
+        redisPublisher.publish(message);
+        log.info("PARTICIPANT_JOINED message publish to redis");
     }
 
     @AsyncTransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
@@ -31,6 +41,9 @@ public class WaitingRoomEventListener {
 
         roomService.createRoom(waitingRoom, participantIds);
 
-        notifier.notifyRoomStarted(waitingRoom.roomId());
+        RoomStartedMessage payload = new RoomStartedMessage(waitingRoom.roomId());
+        redisMessage<RoomStartedMessage> message = new redisMessage<>(
+                "ROOM_STARTED", payload);
+        redisPublisher.publish(message);
     }
 }
