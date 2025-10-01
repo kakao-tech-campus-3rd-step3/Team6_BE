69e9ca3ef70066206ddc099adaa6c0ab8436a59f
KNU-K <knukang334@gmail.com>
2025-10-01 11:28:32 +0900

    feat : 게임 카테고리 매핑 및 권한 검사를 위한 어노테이션 및 리졸버 추가

---
+++ src/main/java/com/icebreaker/be/global/annotation/CurrentGameCategory.java
@@ -0,0 +1,13 @@
+package com.icebreaker.be.global.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CurrentGameCategory {
+
+    String roomCodeParam() default "roomCode";
+}
