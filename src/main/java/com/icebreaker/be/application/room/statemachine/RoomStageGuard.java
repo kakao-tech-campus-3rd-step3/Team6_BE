715bcd666c8154ac3fd15470c35380c3a4f2297a
KNU-K <knukang334@gmail.com>
2025-10-01 11:28:11 +0900

    fix : 불필요한 객체 삭제 (전략패턴에서 일반 구조로 변경)

--- src/main/java/com/icebreaker/be/application/room/statemachine/RoomStageGuard.java
+++
@@ -1,9 +0,0 @@
-package com.icebreaker.be.application.room.statemachine;
-
-import com.icebreaker.be.domain.room.vo.Stage;
-import com.icebreaker.be.domain.room.vo.StageEvent;
-
-public interface RoomStageGuard {
-
-    boolean canTransition(Stage current, StageEvent event);
-}
