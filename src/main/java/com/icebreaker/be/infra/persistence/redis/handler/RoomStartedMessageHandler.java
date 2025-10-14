3800b935f94b5ec376f022e77effdbd1ef1cc65b
eastgyu11 <eastgyu11@naver.com>
2025-10-14 19:36:04 +0900

    feat: RoomStartMessageHandler 추가

---
+++ src/main/java/com/icebreaker/be/infra/persistence/redis/handler/RoomStartedMessageHandler.java
@@ -0,0 +1,28 @@
+package com.icebreaker.be.infra.persistence.redis.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.icebreaker.be.infra.messaging.waitingroom.WaitingRoomWebSocketNotifier;
+import com.icebreaker.be.infra.persistence.redis.message.PubSubMessageType;
+import com.icebreaker.be.infra.persistence.redis.message.RoomStartedMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RoomStartedMessageHandler implements MessageHandler {
+
+    private final WaitingRoomWebSocketNotifier waitingRoomWebSocketNotifier;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public PubSubMessageType getMessageType() {
+        return PubSubMessageType.ROOM_STARTED;
+    }
+
+    @Override
+    public void handleAndSend(Object payload) {
+        RoomStartedMessage startedPayload = objectMapper.convertValue(payload,
+                RoomStartedMessage.class);
+        waitingRoomWebSocketNotifier.notifyRoomStarted(startedPayload.getRoomId());
+    }
+}
