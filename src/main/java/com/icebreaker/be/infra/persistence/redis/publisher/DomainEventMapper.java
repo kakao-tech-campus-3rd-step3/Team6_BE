99c0e489a791d374755fe2d5793b30255aaa68f8
eastgyu11 <eastgyu11@naver.com>
2025-10-14 21:10:59 +0900

    refactor: DomainEventMapper의 불필요한 변수 제거

--- src/main/java/com/icebreaker/be/infra/persistence/redis/publisher/DomainEventMapper.java
+++ src/main/java/com/icebreaker/be/infra/persistence/redis/publisher/DomainEventMapper.java
@@ -1,22 +1,17 @@
 package com.icebreaker.be.infra.persistence.redis.publisher;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.icebreaker.be.application.waitingroom.event.WaitingRoomFullEvent;
 import com.icebreaker.be.application.waitingroom.event.WaitingRoomParticipantJoinedEvent;
 import com.icebreaker.be.infra.persistence.redis.message.ParticipantJoinedMessage;
 import com.icebreaker.be.infra.persistence.redis.message.PubSubMessage;
 import com.icebreaker.be.infra.persistence.redis.message.PubSubMessageType;
 import com.icebreaker.be.infra.persistence.redis.message.RoomStartedMessage;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
 public class DomainEventMapper {
-
-    private final ObjectMapper objectMapper;
 
     public PubSubMessage<?> toPubSubMessage(Object event) {
         if (event instanceof WaitingRoomParticipantJoinedEvent joinedEvent) {
