0fd2ef823938964ab6876bea4155f15f8f715809
KNU-K <knukang334@gmail.com>
2025-10-18 09:25:56 +0900

    feat: add error codes for question generation failure and topic preloading

--- src/main/java/com/icebreaker/be/global/exception/ErrorCode.java
+++ src/main/java/com/icebreaker/be/global/exception/ErrorCode.java
@@ -34,13 +34,14 @@ public enum ErrorCode {
 
     // QUESTION ERROR
     QUESTION_NOT_FOUND("질문을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    QUESTION_GENERATION_FAILED("질문 생성에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
     INVALID_QUESTION_TYPE("유효하지 않은 질문 타입입니다.", HttpStatus.BAD_REQUEST),
-
     // INTEREST ERROR
     INVALID_INTEREST_TYPE("유효하지 않은 관심사 타입입니다.", HttpStatus.BAD_REQUEST),
 
     // GAME ERROR
     INVALID_GAME_CATEGORY("유효하지 않은 게임 카테고리입니다.", HttpStatus.BAD_REQUEST),
+    TOPIC_NOT_PRELOADED("토픽이 사전 로드되지 않았습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
 
     INVALID_JWT_PAYLOAD("유효하지 않은 JWT 페이로드입니다.", HttpStatus.UNAUTHORIZED),
     EXPIRED_JWT_TOKEN("만료된 JWT 토큰입니다.", HttpStatus.UNAUTHORIZED),
