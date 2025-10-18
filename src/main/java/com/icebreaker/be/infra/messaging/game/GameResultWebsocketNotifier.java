2fae36e97d2ca3881682460083ca6d821feb881a
KNU-K <knukang334@gmail.com>
2025-10-18 09:26:15 +0900

    feat: extend GameResultWebsocketNotifier to support user-specific game result notifications

--- src/main/java/com/icebreaker/be/infra/messaging/game/GameResultWebsocketNotifier.java
+++ src/main/java/com/icebreaker/be/infra/messaging/game/GameResultWebsocketNotifier.java
@@ -1,19 +1,35 @@
 package com.icebreaker.be.infra.messaging.game;
 
-import com.icebreaker.be.application.game.GameResult;
-import com.icebreaker.be.application.game.messaging.GameResultNotifier;
+import com.icebreaker.be.application.game.handler.GameResult;
+import com.icebreaker.be.application.game.messaging.GameNotifier;
 import com.icebreaker.be.domain.game.GameCategory;
+import com.icebreaker.be.infra.messaging.AbstractStompNotifier;
 import java.util.List;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
-public class GameResultWebsocketNotifier implements GameResultNotifier {
+public class GameResultWebsocketNotifier extends AbstractStompNotifier implements
+        GameNotifier {
 
-    @Override
-    public void notifyGameResult(String roomCode, GameResult gameResult) {
-        //TODO: WebSocket으로 게임 결과 전송
+    public GameResultWebsocketNotifier(SimpMessagingTemplate messagingTemplate) {
+        super(messagingTemplate);
     }
 
+    @Async
+    @Override
+    public void notifyGameResultToUser(String userId, GameResult gameResult) {
+        sendToUser(userId, "/queue/game-result", gameResult,
+                "게임 결과를 정상적으로 전송했습니다.");
+    }
+
+    @Override
+    public void notifyGameResult(String roomCode, GameResult<?> gameResult) {
+
+    }
+
+    @Async
     @Override
     public void notifyGameList(String roomCode, List<GameCategory> categories) {
         //TODO: WebSocket으로 게임 리스트 전송
