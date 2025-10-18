f3bc7cd12ee0d6bb9d2da417ad275c190d3ea61d
KNU-K <knukang334@gmail.com>
2025-10-18 09:27:48 +0900

    feat: implement QuestionPoolService and QuestionScheduler for question generation and scheduling

---
+++ src/main/java/com/icebreaker/be/infra/scheduler/QuestionScheduler.java
@@ -0,0 +1,29 @@
+package com.icebreaker.be.infra.scheduler;
+
+import com.icebreaker.be.domain.topic.Topic;
+import com.icebreaker.be.domain.user.Interest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class QuestionScheduler {
+
+    private final QuestionAsyncExecutor asyncExecutor;
+
+    @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
+    public void run() {
+        log.info("[QuestionScheduler] 질문 생성 스케줄 시작");
+
+        var allTopics = Interest.getAllDisplayNames().stream()
+                .map(Topic::new)
+                .toList();
+
+        for (Topic topic : allTopics) {
+            asyncExecutor.processTopicAsync(topic);
+        }
+    }
+}
