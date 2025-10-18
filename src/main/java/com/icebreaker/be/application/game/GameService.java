8e256d9562afcb42d719f33e0e435bfe1409b14f
KNU-K <knukang334@gmail.com>
2025-10-18 09:26:21 +0900

    feat: refactor GameService to integrate GameNotifier and TopicService for improved game management

--- src/main/java/com/icebreaker/be/application/game/GameService.java
+++ src/main/java/com/icebreaker/be/application/game/GameService.java
@@ -2,14 +2,9 @@ package com.icebreaker.be.application.game;
 
 import static com.icebreaker.be.domain.game.GameCategory.ALL_GAME_CATEGORIES;
 
-import com.icebreaker.be.application.game.handler.GameHandler;
-import com.icebreaker.be.application.game.messaging.GameResultNotifier;
+import com.icebreaker.be.application.game.messaging.GameNotifier;
 import com.icebreaker.be.application.room.RoomOwnerService;
-import com.icebreaker.be.domain.game.GameCategory;
-import com.icebreaker.be.domain.room.repo.RoomStageRepository;
-import com.icebreaker.be.domain.room.vo.RoomStage;
-import com.icebreaker.be.domain.room.vo.Stage;
-import com.icebreaker.be.global.common.resolver.Resolver;
+import com.icebreaker.be.application.topic.TopicService;
 import com.icebreaker.be.global.exception.BusinessException;
 import com.icebreaker.be.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -21,35 +16,22 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class GameService {
 
-    private final Resolver<GameCategory, GameHandler> gameActionResolver;
-
     private final RoomOwnerService roomOwnerService;
-    private final RoomStageRepository roomStageRepository;
-
-    private final GameResultNotifier notifier;
+    private final TopicService topicService;
+    private final GameManager gameManager;
+    private final GameNotifier notifier;
 
     public void start(String roomCode, Long userId) {
         roomOwnerService.validateRoomOwner(roomCode, userId);
-
-        GameCategory category = resolveCategory(roomCode);
-        GameHandler gameHandler = gameActionResolver.resolve(category);
-        GameResult result = gameHandler.handle();
-
-        notifier.notifyGameResult(roomCode, result);
+        if (!topicService.isTopicPreloaded(roomCode)) {
+            throw new BusinessException(ErrorCode.TOPIC_NOT_PRELOADED);
+        }
+        gameManager.startGame(roomCode);
     }
 
     public void sendGameList(String roomCode, Long userId) {
         roomOwnerService.validateRoomOwner(roomCode, userId);
-
         notifier.notifyGameList(roomCode, ALL_GAME_CATEGORIES);
-    }
-
-    private GameCategory resolveCategory(String roomCode) {
-        Stage currentStage = roomStageRepository.findByRoomCode(roomCode)
-                .map(RoomStage::stage)
-                .orElseThrow(() -> new BusinessException(ErrorCode.ROOM_STAGE_NOT_FOUND));
-
-        return GameCategory.fromStage(currentStage);
     }
 }
 
