fb58c6c0d61bafddad9903e5ebc9ad309e2646fb
KNU-K <knukang334@gmail.com>
2025-10-18 09:25:10 +0900

    feat: add RedisProperties and RedissonConfig for Redis integration

---
+++ src/main/java/com/icebreaker/be/global/config/RedisProperties.java
@@ -0,0 +1,14 @@
+package com.icebreaker.be.global.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "spring.data.redis")
+public class RedisProperties {
+
+    private String host;
+    private int port;
+}
