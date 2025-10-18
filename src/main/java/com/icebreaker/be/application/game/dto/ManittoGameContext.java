b24ad24d45837c57ad3814dff669567c228977c7
KNU-K <knukang334@gmail.com>
2025-10-19 03:36:03 +0900

    feat: add GameContext and ManittoGameContext classes for game context management

---
+++ src/main/java/com/icebreaker/be/application/game/dto/ManittoGameContext.java
@@ -0,0 +1,8 @@
+package com.icebreaker.be.application.game.dto;
+
+public final class ManittoGameContext extends GameContext {
+
+    public ManittoGameContext(String roomCode) {
+        super(roomCode);
+    }
+}
