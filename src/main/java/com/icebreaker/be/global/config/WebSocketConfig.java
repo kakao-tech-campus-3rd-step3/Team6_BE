a6212adceb17a60057726158a02882964bb74f52
KNU-K <knukang334@gmail.com>
2025-10-22 03:13:43 +0900

    feat: implement JwtHandshakeHandler for WebSocket authentication

--- src/main/java/com/icebreaker/be/global/config/WebSocketConfig.java
+++ src/main/java/com/icebreaker/be/global/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.icebreaker.be.global.config;
 
 import com.icebreaker.be.global.interceptor.JwtChannelInterceptor;
+import com.icebreaker.be.global.interceptor.JwtHandshakeHandler;
 import com.icebreaker.be.global.resolver.CurrentUserArgumentResolver;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +23,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final CurrentUserArgumentResolver currentUserArgumentResolver;
     private final JwtChannelInterceptor jwtChannelInterceptor;
+    private final JwtHandshakeHandler jwtHandshakeHandler;
     private final WebSocketHeartbeatProperties properties;
 
     @Override
@@ -35,6 +37,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
+                .setHandshakeHandler(jwtHandshakeHandler)
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
